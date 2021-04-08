b482b3b441879f67558c00b35ff3c6a0e80e139d
AllanCapistrano <allan.capistrano3@gmail.com>
2021-04-08 14:40:56 -0300

    Fix factory.

--- database/factories/NodeMCU/ScheduleFactory.php
+++ database/factories/NodeMCU/ScheduleFactory.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Database\Factories;
+namespace Database\Factories\NodeMCU;
 
-use App\Models\Schedule;
+use App\Models\NodeMCU\Schedule;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ScheduleFactory extends Factory
