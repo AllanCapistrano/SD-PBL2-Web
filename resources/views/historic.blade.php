d74366c9e01db733a0bfd787bd81343e3f3f789b
AllanCapistrano <allan.capistrano3@gmail.com>
2021-04-15 14:36:46 -0300

    Change table.

--- resources/views/historic.blade.php
+++ resources/views/historic.blade.php
@@ -9,35 +9,36 @@
 
 
 @section('content')
+    <div class="row mt-5">
+        <div class="col-12 d-flex justify-content-center align-items-center">
+            <h3 style="color: #fff">Tarifa Mensal: R$ 0,38</h3>
+        </div>
+    </div>
     <div class="row">
         <div class="col-8 offset-2 d-flex justify-content-center align-items-center">
             <table class="table table-borderless mt-5">
                 <thead>
                   <tr>
-                    <th scope="col">Mês</th>
+                    <th scope="col">Data</th>
                     <th scope="col">Consumo Total</th>
-                    <th scope="col">Tarifa</th>
                     <th scope="col">Preço Total</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <th scope="row">Jan</th>
+                    <th scope="row">12/04/2021</th>
                     <th>1000 Wh</th>
-                    <th>R$ 0,32</th>
-                    <th>R$ 320,00</th>
+                    <th>R$ 380,00</th>
                   </tr>
                   <tr>
-                    <th scope="row">Fev</th>
+                    <th scope="row">13/04/2021</th>
                     <th>920 Wh</th>
-                    <th>R$ 0,38</th>
                     <th>R$ 349,60</th>
                   </tr>
                   <tr>
-                    <th scope="row">Mar</th>
+                    <th scope="row">14/04/2021</th>
                     <th>896 Wh</th>
-                    <th>R$ 0,43</th>
-                    <th>R$ 385,25</th>
+                    <th>R$ 340,40</th>
                   </tr>
                 </tbody>
             </table>
