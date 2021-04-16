a8141cbde1e120945fcfc0b283d7b8b4675ad80e
JoaoErick <jerick1415@outlook.com>
2021-04-16 19:01:53 -0300

    Fix footer

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -67,11 +67,11 @@
       <div class="container">
         <div class="row footer-row align-items-center justify-content-center">
           <div class="col-12 footer-col d-flex justify-content-center align-items-center">
-            <a href="https://github.com/EcompJr" target="_blank">
+            <a href="https://github.com/AllanCapistrano" target="_blank">
               <i class="fab fa-github-square"></i> 
               <span class="author-footer">Allan Capistrano</span>
             </a>
-            <a href="https://github.com/EcompJr" target="_blank">
+            <a href="https://github.com/JoaoErick" target="_blank">
               <i class="fab fa-github-square"></i>
               <span class="author-footer">João Erick Barbosa</span>
             </a>
